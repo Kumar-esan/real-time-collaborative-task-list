e721a2e31d221fa7062f8eebf01a11e8204c90a3
kumaresan <108225740+Kumar-esan@users.noreply.github.com>
2026-03-12 12:42:42 +0000

    init

---
+++ my-next-app/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Real Time Collaborative Task List",
+  description: "A real-time collaborative task management application built with React and WebSockets that allows multiple users to manage and synchronize tasks instantly.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
